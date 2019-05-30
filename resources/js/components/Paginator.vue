295880154827da70e43cd2a51480bce88e454800
Usama Muneer <info@usamamuneer.me>
2019-05-30 09:03:28 +0500

    Paginator component

---
+++ resources/js/components/Paginator.vue
@@ -0,0 +1,67 @@
+<template>
+    <ul class="pagination">
+        <li v-if="pagination.current_page > 1" class="page-item" >
+            <a href="javascript:void(0)" aria-label="Previous" v-on:click.prevent="changePage(pagination.current_page - 1)">
+                <span aria-hidden="true">«</span>
+            </a>
+        </li>
+        <li v-for="page in pagesNumber" :class="{'active': page == pagination.current_page}">
+            <a href="javascript:void(0)" v-on:click.prevent="changePage(page)">{{ page }}</a>
+        </li>
+        <li v-if="pagination.current_page < pagination.last_page">
+            <a href="javascript:void(0)" aria-label="Next" v-on:click.prevent="changePage(pagination.current_page + 1)">
+                <span aria-hidden="true">»</span>
+            </a>
+        </li>
+    </ul>
+</template>
+<script>
+    export default{
+        props: {
+            pagination: {
+                type: Object,
+                required: true
+            },
+            offset: {
+                type: Number,
+                default: 4
+            }
+        },
+        computed: {
+            pagesNumber() {
+                if (!this.pagination.to) {
+                    return [];
+                }
+                let from = this.pagination.current_page - this.offset;
+                if (from < 1) {
+                    from = 1;
+                }
+                let to = from + (this.offset * 2);
+                if (to >= this.pagination.last_page) {
+                    to = this.pagination.last_page;
+                }
+                let pagesArray = [];
+                for (let page = from; page <= to; page++) {
+                    pagesArray.push(page);
+                }
+                return pagesArray;
+            }
+        },
+        methods : {
+            changePage(page) {
+                this.pagination.current_page = page;
+                this.$emit('paginate');
+            }
+        }
+    }
+</script>
+<style>
+    .pagination > .active > a, .pagination > .active > a:focus{
+        border-color:#004590;
+        background-color:red;
+    }
+    .pagination {
+        list-style: none;
+        display: inline-flex
+    }
+</style>
